609a198f40ebca6f651861c49cf602c2dbdaae2d
M A M A R E Z A <rrealmrezarajabi@gmail.com>
2026-09-07 11:16:39 +0330

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.js
@@ -0,0 +1,21 @@
+class Solution {
+    
+    hasDuplicate(nums) {
+       
+        const seen = new Set()
+
+        for (let num of nums){
+
+            if(seen.has(num)){
+                return true
+            }
+
+            seen.add(num)
+        }
+
+
+        return false
+
+
+    }
+}
